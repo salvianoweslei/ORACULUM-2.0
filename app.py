7811aaf4c71c9ce10f93217e26ca0a079260b694
salvianoweslei <160778794+salvianoweslei@users.noreply.github.com>
2025-06-28 11:17:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 TELEGRAM_TOKEN = "7759153655:AAF3sb4J106-_B3WdOUhbJGOw3cg9zQHLQk"
 CHAT_ID = "-4974125255"
-GOOGLE_SHEETS_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbw9Ot6CdTZZM8Sj53Emr5LXNZS2ufN3oGJCtw2PUnFjl8KtHC11SnwtIwASyyVkB5Ya/exec"
+GOOGLE_SHEETS_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbytU0_lTRmANdocYpqFJYjKSROGRWoe_xBRYSXMVkztXsPAoxV-yFgLvACDbCG3XiEG/exec
+"
 
 signal_buffer = {}
 active_signals = {}
